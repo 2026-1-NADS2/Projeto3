b1b9c976d67b5a14ee23ea204ee2e649b60d1107
Leonardo Lamari <160190496+LeonardoLamar12@users.noreply.github.com>
2026-05-11 21:41:35 -0300

    Delete documentos/Entrega 2/POO/POO2 directory

--- documentos/Entrega 2/POO/POO2/Models/Comprador.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-namespace POO2.Models
-{
-    public class Comprador : Usuario
-    {
-        [JsonIgnore]
-        public double saldo { get; set; }
-    }
-}
--- documentos/Entrega 2/POO/POO2/Models/Vendedor.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.Json.Serialization;
-namespace POO2.Models
-{
-    public class Vendedor : Usuario
-    {
-        [JsonIgnore]
-        public List<Anuncio> anuncios { get; set; } = new List<Anuncio>();
-    }
-}
